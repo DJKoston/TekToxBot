abe62938adf7eff92c9621b8c9f312affdfee90f
Haydon Koston <haydonkoston@hotmail.co.uk>
2021-02-07 22:29:34 +0000

    Added Event Commands

--- TekTox.Bot/Bot.cs
+++ TekTox.Bot/Bot.cs
@@ -1,15 +1,22 @@
 ﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
+using Microsoft.Extensions.DependencyInjection;
 using DSharpPlus.Interactivity;
 using DSharpPlus.Interactivity.Extensions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TekTox.Bot.Commands;
+using TekTox.Core.Services;
+using TekTox.DAL;
+using TekTox.DAL.Models;
+using System.Threading;
 
 namespace TekTox.Bot
 {
@@ -21,6 +28,8 @@ namespace TekTox.Bot
 
         public Bot(IServiceProvider services, IConfiguration configuration)
         {
+            _eventListService = services.GetService<IEventListService>();
+
             var token = configuration["token"];
             var prefix = configuration["prefix"];
 
@@ -68,8 +77,61 @@ namespace TekTox.Bot
             Console.ResetColor();
         }
 
+        private readonly IEventListService _eventListService;
+
         private Task OnHeartbeat(DiscordClient sender, HeartbeatEventArgs e)
         {
+            new Thread(async () =>
+            {
+                var allEvents = _eventListService.ListEvents();
+
+                if (allEvents == null) { return; }
+
+                foreach (EventList anEvent in allEvents)
+                {
+                    var eventDateTime = anEvent.DateTime;
+                    var parsedDate = DateTime.Parse(eventDateTime);
+
+                    var fiveDayWarningDate = parsedDate.AddDays(-5);
+                    var oneDayWarningDate = parsedDate.AddDays(-1);
+                    var tenMinWarning = parsedDate.AddMinutes(-10);
+
+                    var embed = new DiscordEmbedBuilder
+                    {
+                        Title = "Time Conversions:",
+                        Description = $"{parsedDate.AddHours(-7).ToShortTimeString()} (MST)\n\n{parsedDate.AddHours(-6).ToShortTimeString()} (CST)\n\n{parsedDate.ToShortTimeString()} (GMT)",
+                        Color = DiscordColor.Yellow
+                    };
+
+                    if (DateTime.Now.Day == fiveDayWarningDate.Day && DateTime.Now.Month == fiveDayWarningDate.Month && DateTime.Now.Year == fiveDayWarningDate.Year && DateTime.Now.Hour == fiveDayWarningDate.Hour && DateTime.Now.Minute == fiveDayWarningDate.Minute)
+                    {
+                        var guild = await sender.GetGuildAsync(804875172157980704);
+
+                        var channel = guild.GetChannel(804875175883178076);
+
+                        await channel.SendMessageAsync($"Reminder: {anEvent.Attendees} don't forget!\n\nYou are Scheduled for TekTox recording on {parsedDate.ToLongDateString()} at {parsedDate.AddHours(-6).ToShortTimeString()} (CST) to discuss {anEvent.EventName}.\n\nLooking forward to seeing you there.\n\nDon't forget to give at least 48 hrs notice if you cannot make it.", embed: embed).ConfigureAwait(false);
+                    }
+
+                    if (DateTime.Now.Day == oneDayWarningDate.Day && DateTime.Now.Month == oneDayWarningDate.Month && DateTime.Now.Year == oneDayWarningDate.Year && DateTime.Now.Hour == oneDayWarningDate.Hour && DateTime.Now.Minute == oneDayWarningDate.Minute)
+                    {
+                        var guild = await sender.GetGuildAsync(804875172157980704);
+
+                        var channel = guild.GetChannel(804875175883178076);
+
+                        await channel.SendMessageAsync($"Reminder: {anEvent.Attendees} don't forget!\n\nYou are Scheduled for TekTox recording tomorrow at {parsedDate.AddHours(-6).ToShortTimeString()} (CST) to discuss {anEvent.EventName}.\n\nLooking forward to seeing you there.\n\nYou should have already have given us your notice if you cannot make it by this point.", embed: embed).ConfigureAwait(false);
+                    }
+
+                    if (DateTime.Now.Day == tenMinWarning.Day && DateTime.Now.Month == tenMinWarning.Month && DateTime.Now.Year == tenMinWarning.Year && DateTime.Now.Hour == tenMinWarning.Hour && DateTime.Now.Minute == tenMinWarning.Minute)
+                    {
+                        var guild = await sender.GetGuildAsync(804875172157980704);
+
+                        var channel = guild.GetChannel(804875175883178076);
+
+                        await channel.SendMessageAsync($"Reminder: {anEvent.Attendees} don't forget!\n\nYou are Scheduled for TekTox recording in 10 minutes to discuss {anEvent.EventName}.\n\nLooking forward to seeing you there.\n\nIf you can't make it, please DM Dude ASAP.").ConfigureAwait(false);
+                    }
+                }
+            });
+
             return Task.CompletedTask;
         }
     }
--- TekTox.Bot/Commands/EventCommands.cs
+++ TekTox.Bot/Commands/EventCommands.cs
@@ -1,7 +1,10 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
 using System;
+using System.Data.Entity;
+using System.Linq;
 using System.Threading.Tasks;
 using TekTox.Bot.Handlers.Dialogue;
 using TekTox.Bot.Handlers.Dialogue.Steps;
@@ -27,11 +30,11 @@ namespace TekTox.Bot.Commands
         {
             var thirdStep = new TextStep("Who will be taking part in this event?", null);
             var secondStep = new TextStep("What is the Event name?", thirdStep);
-            var firstStep = new DateTimeStep("What is the date you wish this event to take place on?", secondStep);
+            var firstStep = new DateTimeStep("What is the date/time you wish this event to take place on?\nStyle of Response Needed: DD/MM/YYYY HH:MM", secondStep);
 
             var newEvent = new EventList();
 
-            firstStep.OnValidResult += (result) => newEvent.DateTime = $"{result}"; 
+            firstStep.OnValidResult += (result) => newEvent.DateTime = $"{result}";
             secondStep.OnValidResult += (result) => newEvent.EventName = $"{result}";
             thirdStep.OnValidResult += (result) => newEvent.Attendees = $"{result}";
 
@@ -46,13 +49,64 @@ namespace TekTox.Bot.Commands
 
             if (!succeeded) { return; }
 
+            await _eventService.CreateNewEvent(newEvent).ConfigureAwait(false);
+
+            var parsedTimeDate = DateTime.Parse(newEvent.DateTime).AddHours(-6);
+
             var embed = new DiscordEmbedBuilder
             {
-                Title = "See below for an example of the data being moved to the database.",
-                Description = $"Date/Time: {newEvent.DateTime}\n\nEvent Name: {newEvent.EventName}\n\nAtendees: {newEvent.Attendees}"
+                Title = "Event Created:",
+                Color = DiscordColor.DarkRed,
+                Description = $"Date/Time: {parsedTimeDate.ToShortDateString()} at {parsedTimeDate.ToShortTimeString()}\n\nEvent Name: {newEvent.EventName}\n\nAttendees: {newEvent.Attendees}",
             };
+            embed.WithFooter($"Event ID: {newEvent.Id}");
+
+            var eventChannel = ctx.Guild.GetChannel(808076578725822474);
 
             await ctx.Channel.SendMessageAsync(embed: embed).ConfigureAwait(false);
+            await eventChannel.SendMessageAsync(embed: embed).ConfigureAwait(false);
+        }
+
+        [Command("delete")]
+        public async Task DeleteEvent(CommandContext ctx, int eventId)
+        {
+            var selectedEvent = _context.EventLists.Where(x => x.Id == eventId).First();
+
+            var embed = new DiscordEmbedBuilder
+            {
+                Title = "Event Found:",
+                Color = DiscordColor.DarkRed,
+                Description = $"Date/Time: {selectedEvent.DateTime}\n\nEvent Name: {selectedEvent.EventName}\n\nAttendees: {selectedEvent.Attendees}",
+            };
+            embed.WithFooter($"Event ID: {selectedEvent.Id}");
+
+            var confirmMessage = await ctx.Channel.SendMessageAsync("Please confirm you wish to delete this event", embed: embed).ConfigureAwait(false);
+
+            var thumbsupEmoji = DiscordEmoji.FromName(ctx.Client, ":thumbsup:");
+            var thumbsdownEmoji = DiscordEmoji.FromName(ctx.Client, ":thumbsdown:");
+
+            await confirmMessage.CreateReactionAsync(thumbsupEmoji).ConfigureAwait(false);
+            await confirmMessage.CreateReactionAsync(thumbsdownEmoji).ConfigureAwait(false);
+
+            var interactivity = ctx.Client.GetInteractivity();
+
+            var reactionResult = await interactivity.WaitForReactionAsync(x => x.Message == confirmMessage && x.User == ctx.User && (x.Emoji == thumbsupEmoji || x.Emoji == thumbsdownEmoji));
+
+            if(reactionResult.Result.Emoji == thumbsupEmoji)
+            {
+                await confirmMessage.DeleteAsync();
+
+                await _eventService.DeleteEvent(selectedEvent);
+
+                await ctx.Channel.SendMessageAsync("Event Deleted!").ConfigureAwait(false);
+            }
+
+            else if(reactionResult.Result.Emoji == thumbsdownEmoji)
+            {
+                await confirmMessage.DeleteAsync();
+
+                await ctx.Channel.SendMessageAsync("Cancelled deleting the event.").ConfigureAwait(false);
+            }
         }
     }
 }
