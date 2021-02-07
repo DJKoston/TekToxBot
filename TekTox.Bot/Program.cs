959ac8b79de8c795a35e346e21c060e164f799a1
Haydon Koston <haydonkoston@hotmail.co.uk>
2021-02-07 04:05:26 +0000

    Add project files.

---
+++ TekTox.Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TekTox.Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
